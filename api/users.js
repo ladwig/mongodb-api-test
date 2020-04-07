7202bd6999e8bd2ea2bdf450ac7138b4f376fd45
daniel <ladwig.daniel@icloud.com>
2020-04-07 20:53:16 +0200

    test

--- api/users.js
+++ api/users.js
@@ -34,11 +34,11 @@ module.exports = async (req, res) => {
   const db = await connectToDatabase(process.env.MONGODB_URI)
 
   // Select the "users" collection from the database
-  const collection = await db.collection('sample_weatherdata.data')
+  const collection = await db.collection('sample_weatherdata')
 
   // Select the users collection from the database
   const id = await collection.find({}).toArray()
 
   // Respond with a JSON string of all users in the collection
-  res.status(200).json({ id.id })
+  res.status(200).json({ id })
 }
