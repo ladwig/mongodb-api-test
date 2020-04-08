b5999681c5a708a4753ce11aa0b1cb0361076d12
daniel <ladwig.daniel@icloud.com>
2020-04-08 16:41:10 +0200

    Added a second endpoint

--- api/habits.js
+++ api/habits.js
@@ -34,7 +34,7 @@ module.exports = async (req, res) => {
   const db = await connectToDatabase(process.env.MONGODB_URI)
 
   // Select the "users" collection from the database
-  const collection = await db.collection('users')
+  const collection = await db.collection('theaters')
 
   // Select the users collection from the database
   const data = await collection.find({}).toArray()
